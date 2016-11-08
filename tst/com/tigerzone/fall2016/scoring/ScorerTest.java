38d14f18d20469fc55424aefbce0848be12f0152
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-11-07 20:22:50 -0500

    Add test directory for unit tests

---
+++ tst/com/tigerzone/fall2016/scoring/ScorerTest.java
@@ -0,0 +1,24 @@
+package com.tigerzone.fall2016.scoring;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by matthewdiaz on 11/7/16.
+ */
+public class ScorerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUpdateScore() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAnnounceWinner() throws Exception {
+
+    }
+}
