ff478886be0efe284c6a18c068431d087f2c61d2
Jeff G <drak@ufl.edu>
2016-11-10 22:42:20 -0500

    Added some functionality to GameSystem, TextFilePort, TileStack, etc. Added more test markers, too.

---
+++ tst/com/tigerzone/fall2016/gamesystem/GameSystemTest.java
@@ -0,0 +1,26 @@
+package com.tigerzone.fall2016.gamesystem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jeff on 2016/11/10.
+ */
+public class GameSystemTest {
+
+    @Test
+    public void testAcceptTurn() throws Exception {
+
+    }
+
+    @Test
+    public void testStartGame() throws Exception {
+
+    }
+
+    @Test
+    public void testInitializeGame() throws Exception {
+
+    }
+}
---
+++ tst/com/tigerzone/fall2016/gamesystem/TileStackTest.java
@@ -0,0 +1,21 @@
+package com.tigerzone.fall2016.gamesystem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jeff on 2016/11/10.
+ */
+public class TileStackTest {
+
+    @Test
+    public void testShuffle() throws Exception {
+
+    }
+
+    @Test
+    public void testPop() throws Exception {
+
+    }
+}
