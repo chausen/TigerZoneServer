282b9b9c0199998959e48dd0c88fb5d8650547a9
Jeff G <drak@ufl.edu>
2016-11-13 21:54:57 -0500

    Small change to TextFilePort.

---
+++ src/com/tigerzone/fall2016/ports/NetworkPort.java
@@ -0,0 +1,25 @@
+package com.tigerzone.fall2016.ports;
+
+import com.tigerzone.fall2016.adapters.PlayerOutAdapter;
+import com.tigerzone.fall2016.gamesystem.Turn;
+import com.tigerzone.fall2016.tileplacement.tile.AreaTile;
+
+/**
+ * Created by Jeff on 2016/11/13.
+ */
+public class NetworkPort implements PlayerOutAdapter {
+    @Override
+    public void passTurn(Turn lastturn, AreaTile areatile) {
+
+    }
+
+    @Override
+    public void notifyBeginGame(AreaTile areatile) {
+
+    }
+
+    @Override
+    public void notifyEndGame() {
+
+    }
+}
