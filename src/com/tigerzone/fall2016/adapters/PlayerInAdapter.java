3cbf4c12d7ddceda8690490f4cbed29349816e7d
mistiansen <echristiansen@ufl.edu>
2016-11-18 16:38:12 -0500

    push

--- src/com/tigerzone/fall2016/adapters/PlayerInAdapter.java
+++ src/com/tigerzone/fall2016/adapters/PlayerInAdapter.java
@@ -2,10 +2,13 @@ package com.tigerzone.fall2016.adapters;
 import com.tigerzone.fall2016.gamesystem.Turn;
 import com.tigerzone.fall2016.tileplacement.tile.PlayableTile;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 
 public interface PlayerInAdapter
 {
-    public void initializeGame(String player1id, String player2id, long seed);
+    public void initializeGame(String player1id, String player2id, LinkedList<PlayableTile> playableTiles);
     public void setOutAdapter(PlayerOutAdapter outAdapter);
     public void receiveTurn(Turn t);
     //TODO: Do we need this?
--- src/com/tigerzone/fall2016server/tournament/Challenge.java
+++ src/com/tigerzone/fall2016server/tournament/Challenge.java
@@ -1,9 +1,29 @@
 package com.tigerzone.fall2016server.tournament;
 
+import com.tigerzone.fall2016.gamesystem.Player;
+import com.tigerzone.fall2016.tileplacement.tile.PlayableTile;
+
+import java.util.LinkedList;
+import java.util.Set;
+
 /**
  * Created by lenovo on 11/17/2016.
  */
 public class Challenge {
+
+    LinkedList<PlayableTile> tiles;
+    Set<Player> players;
+
+    public Challenge(String[] tiles, long seed) {
+        this.tiles = TileStackGenerator.generateTiles(tiles, seed);
+    }
+
+
+    public int getPlayerCount() {
+        int playerCount = players.size();
+        return playerCount;
+    }
+
 
 
 
