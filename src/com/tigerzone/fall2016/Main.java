b2907a8c8be0940982103b007723f88a4c22d5ce
chausen <clayhausen@gmail.com>
2016-11-18 17:17:32 -0500

    Removed duplicate code in IOPort

--- src/com/tigerzone/fall2016/Main.java
+++ src/com/tigerzone/fall2016/Main.java
@@ -11,6 +11,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]){
         CmdPromptInterface cmd = new CmdPromptInterface();
-        cmd.startGame();
+//        cmd.startGame();
     }
 }
