2cb4a4de3ac1f9a20fc7573095f99edbb698c4de
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-11-26 15:40:09 -0500

    Added generateMessageToActivePlayer in GameToClientMessageFormatter cleaned out IOPort a bit

--- src/com/tigerzone/fall2016server/server/protocols/GameToClientMessageFormatter.java
+++ src/com/tigerzone/fall2016server/server/protocols/GameToClientMessageFormatter.java
@@ -16,4 +16,20 @@ public class GameToClientMessageFormatter {
         stringBuffer.append(message);
         return stringBuffer.toString();
     }
+
+    public static String generateMessageToActivePlayer(int gameID, int maxTime, int moveNum, String tile){
+        String secondOrSeconds = (maxTime == 1) ? " SECOND" : " SECONDS";
+
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("MAKE YOUR MOVE IN GAME ");
+        stringBuffer.append(gameID);
+        stringBuffer.append(" WITHIN ");
+        stringBuffer.append(maxTime);
+        stringBuffer.append(secondOrSeconds);
+        stringBuffer.append(":MOVE ");
+        stringBuffer.append(moveNum);
+        stringBuffer.append(" PLACE ");
+        stringBuffer.append(tile);
+        return stringBuffer.toString();
+    }
 }
