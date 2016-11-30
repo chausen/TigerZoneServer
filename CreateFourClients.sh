ce84c5b4c920d204484bc9ce1184118b518513ed
chausen <clayhausen@gmail.com>
2016-11-29 20:14:48 -0500

    Fixed bug where losses weren't being incremented on a forfeit

--- CreateFourClients.sh
+++ CreateFourClients.sh
@@ -2,4 +2,4 @@
 java -classpath out/production/TigerZone com.tigerzone.fall2016server.client.ParameterizedClientMain TEAMA  IAMA Player1Moves.txt Player2Moves.txt localhost 4444 &
 java -classpath out/production/TigerZone com.tigerzone.fall2016server.client.ParameterizedClientMain TEAMB  IAMB Player1Moves.txt Player2Moves.txt localhost 4444 &
 java -classpath out/production/TigerZone com.tigerzone.fall2016server.client.ParameterizedClientMain TEAMC  IAMC Player1Moves.txt Player2Moves.txt localhost 4444 &
-java -classpath out/production/TigerZone com.tigerzone.fall2016server.client.ParameterizedClientMain TEAMD  IAMD Player1Moves.txt MalformedMoves.txt localhost 4444
+java -classpath out/production/TigerZone com.tigerzone.fall2016server.client.ParameterizedClientMain TEAMD  IAMD Player1Moves.txt Player2Moves.txt  localhost 4444
