7c9d7caed92c3a0ee4d9e469e14227a0bc4a4b74
mistiansen <echristiansen@ufl.edu>
2016-12-01 14:42:23 -0500

    sfddsf

--- src/com/tigerzone/fall2016/gamesystem/GameSystem.java
+++ src/com/tigerzone/fall2016/gamesystem/GameSystem.java
@@ -67,7 +67,7 @@ public class GameSystem implements PlayerInAdapter {
 //        ts = new TileStack(getUnplaceableTestStack());
 //        ts.truncateTS(4);
 
-       ts.truncateTS(12); // TODO: 11/27/2016 Change this truncation value for different tests
+       ts.truncateTS(20); // TODO: 11/27/2016 Change this truncation value for different tests
 
         currentTile = ts.peek();
     }
