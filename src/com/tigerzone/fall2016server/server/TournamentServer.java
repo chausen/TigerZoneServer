49349385ecf3a42cedd5b5d9a259348443135ac7
dnkllhr <dnkllhr@ufl.edu>
2016-12-01 21:26:25 -0500

    fixed passed message

--- src/com/tigerzone/fall2016server/server/TournamentServer.java
+++ src/com/tigerzone/fall2016server/server/TournamentServer.java
@@ -19,7 +19,7 @@ public class TournamentServer {
 
     private static int PORT = 4444;
     private static int seed = 123456789;
-    private static int MAX_CONNECTIONS = 2;
+    private static int MAX_CONNECTIONS = 6;
     private static int tournamentID = 1;
     private final int numOfChallenges = 1; //the number of challenges is actually this plus 1
 
