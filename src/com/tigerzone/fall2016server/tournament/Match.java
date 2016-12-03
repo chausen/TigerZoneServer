5087ad45d4de7d40b03b01787762e4f9080e634e
mistiansen <echristiansen@ufl.edu>
2016-12-03 14:34:37 -0500

    pull

--- src/com/tigerzone/fall2016server/tournament/Match.java
+++ src/com/tigerzone/fall2016server/tournament/Match.java
@@ -150,7 +150,6 @@ public class Match extends Thread {
                     sendGameMessage(gameResponse);
                 }
             }
-
             //swap who is the active player in each game
             swapPlayers();
             //Increment move count
